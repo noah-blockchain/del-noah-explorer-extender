7a23b1629053208903c3d418443664c3abbe696c
Noah GO Developer <developer.chain.2019@hotmail.com>
2019-11-02 14:57:36 +0400

    hot fix

--- core/main.go
+++ core/main.go
@@ -153,6 +153,11 @@ func (ext *Extender) Run() {
 	}
 
 	for {
+		if height == 463645 {
+			height ++
+			continue
+		}
+
 		start := time.Now()
 		ext.findOutChasingMode(height)
 		//Pulling block data
